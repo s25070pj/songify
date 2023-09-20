278f4a3504c36740b90f0c68dac191abdd0d30b4
brash <s25070@pjwstk.edu.pl>
2023-09-20 19:14:47 +0200

    adding Lombok and requestId to getSongById

--- src/main/java/com/songify/controller/SongsController.java
+++ src/main/java/com/songify/controller/SongsController.java
@@ -2,12 +2,10 @@ package com.songify.controller;
 
 import com.songify.model.SingleSongResponseDto;
 import com.songify.model.SongResponseDto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +13,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@Log4j2
 public class SongsController {
 
     Map<Integer, String> database = new HashMap<>();
@@ -39,7 +38,8 @@ public class SongsController {
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer  id){
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer  id, @RequestHeader(required = false) String requestId){
+        log.info(requestId);
         String song = database.get(id);
         if (song == null){
             return ResponseEntity.notFound().build();
