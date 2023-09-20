585c5d8552b2a3a15545b2045faa45a08da242db
brash <s25070@pjwstk.edu.pl>
2023-09-20 18:04:14 +0200

    getAllSongs with limit (request param

--- src/main/java/com/songify/controller/SongsController.java
+++ src/main/java/com/songify/controller/SongsController.java
@@ -2,14 +2,17 @@ package com.songify.controller;
 
 import com.songify.model.SingleSongResponseDto;
 import com.songify.model.SongResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class SongsController {
@@ -17,11 +20,22 @@ public class SongsController {
     Map<Integer, String> database = new HashMap<>();
 
     @GetMapping("/songs")
-    public ResponseEntity<SongResponseDto> getAllSongs(){
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit){
         database.put(1, "shawnmendes song1");
         database.put(2, "ariana grande song2");
+        database.put(3, "arfqfq2");
+        database.put(4, "arirety34hy42");
+        if (limit != null) {
+            Map<Integer, String> limitedMap = database.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongResponseDto response = new SongResponseDto(limitedMap);
+            return ResponseEntity.ok(response);
+        }
         SongResponseDto response = new SongResponseDto(database);
         return ResponseEntity.ok(response);
+
     }
 
     @GetMapping("/songs/{id}")
@@ -33,5 +47,6 @@ public class SongsController {
         SingleSongResponseDto response = new SingleSongResponseDto(song);
         return ResponseEntity.ok(response);
     }
+
 
 }
